0184586fd8b4260d1d93c5734f7dbb677638cb85
Jonas Nygaard <jonasnygaard96@gmail.com>
2026-02-10 13:54:02 +0100

    :

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../prisma/generated/client"; // juster path hvis du vil
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error", "warn"],
   });
 
